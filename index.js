3116c7f147553a6b6da9e8e0ba92370c79541361
aliquiop1-ai <ali.quiop1@gmail.com>
2025-11-11 00:44:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors({ origin: "*" }));
 app.use(require("morgan")("dev"));
 
 const emailData = {
-  user: "ali.quiop1@gmail.com",
-  pass: "jdyo heid bpub eudh",
+  user: "irppri724@gmail.com",
+  pass: "mhur rdap aboa wmmg",
   // user: "saudiabsher1990@gmail.com",
   // pass: "qlkg nfnn xaeq fitz",
 };
